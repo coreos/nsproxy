2720f78dc83126a0f4582cda157940222e51e2e2
Timo Derstappen <teemow@gmail.com>
2014-01-17 11:00:18 +0000

    added namespace by path support

--- nsproxy.go
+++ nsproxy.go
@@ -27,8 +27,9 @@ func proxyConn(conn *net.Conn, addr string) {
 func proxy(c *cli.Context) {
 	addr := c.String("addr")
 	target := c.Int("target")
-	if target == 0 {
-		fmt.Fprintln(os.Stderr, "error: a target pid is required")
+	namespacePath := c.String("namespace")
+	if target == 0 && namespacePath == "" {
+		fmt.Fprintln(os.Stderr, "error: a target pid or namespace path is required")
 		return
 	}
 
@@ -41,7 +42,7 @@ func proxy(c *cli.Context) {
 	peers := trimsplit(c.String("peers"), ",")
 	client := etcd.NewClient(peers)
 
-	log.Printf("Proxying for keys in %s on host %s in namespace %d", path, addr, target)
+	log.Printf("Proxying for keys in %s on host %s", path, addr)
 
 	// Keep an eye on the backend path
 	b := backends{path: path}
@@ -52,7 +53,14 @@ func proxy(c *cli.Context) {
 
 	go b.Watch(client)
 
-	listener, err := nameNet.ListenNamespace(uintptr(target), "tcp", addr)
+	var listener net.Listener
+	if target != 0 {
+		log.Printf("Proxying for keys in %s on host %s in namespace %d", path, addr, target)
+		listener, err = nameNet.ListenProcessNamespace(uintptr(target), "tcp", addr)
+	} else {
+		log.Printf("Proxying for keys in %s on host %s in namespace %d", path, addr, namespacePath)
+		listener, err = nameNet.ListenNamespace(namespacePath, "tcp", addr)
+	}
 	if err != nil {
 		panic(err)
 	}
@@ -71,7 +79,7 @@ func proxy(c *cli.Context) {
 			continue
 		}
 
-		log.Printf("PROXY: targetPid:%d targetAddr:%v addr:%v\n", target, addr, next)
+		log.Printf("PROXY: targetAddr:%v addr:%v\n", addr, next)
 		go proxyConn(&conn, next)
 	}
 }
@@ -85,6 +93,7 @@ func main() {
 		cli.StringFlag{"path, p", "", "path to a dir", false},
 		cli.StringFlag{"addr, a", "127.0.0.1:8080", "target address inside the namespace", false},
 		cli.StringFlag{"peers, C", "http://127.0.0.1:4001", "a comma seperated list of machine addresses in the cluster", false},
+		cli.StringFlag{"namespace, ns", "", "target namespace path", false},
 	}
 	app.Action = proxy
 	app.Run(os.Args)
