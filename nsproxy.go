c706d4b336fb2234fd531b0f885c3185a97ed152
Timo Derstappen <teemow@gmail.com>
2014-01-17 22:00:50 +0000

    changed parameters to ns-path/ns-pid

--- nsproxy.go
+++ nsproxy.go
@@ -10,6 +10,7 @@ import (
 	"github.com/codegangsta/cli"
 	"github.com/coreos/go-etcd/etcd"
 	nameNet "github.com/coreos/go-namespaces/net"
+	"github.com/coreos/go-namespaces/namespace"
 )
 
 func proxyConn(conn *net.Conn, addr string) {
@@ -26,10 +27,10 @@ func proxyConn(conn *net.Conn, addr string) {
 
 func proxy(c *cli.Context) {
 	addr := c.String("addr")
-	target := c.Int("target")
-	namespacePath := c.String("namespace")
-	if target == 0 && namespacePath == "" {
-		fmt.Fprintln(os.Stderr, "error: a target pid or namespace path is required")
+	namespacePid := c.Int("namespace-pid")
+	namespacePath := c.String("namespace-path")
+	if namespacePid == 0 && namespacePath == "" {
+		fmt.Fprintln(os.Stderr, "error: a namespace pid or path is required")
 		return
 	}
 
@@ -37,6 +38,14 @@ func proxy(c *cli.Context) {
 	if path == "" {
 		fmt.Fprintln(os.Stderr, "error: an etcd path is required")
 		return
+	}
+
+	if namespacePid != 0 {
+		p, err := namespace.ProcessPath(namespacePid, namespace.CLONE_NEWNET)
+		if err != nil {
+			panic(err)
+		}
+		namespacePath = p
 	}
 
 	peers := trimsplit(c.String("peers"), ",")
@@ -53,14 +62,9 @@ func proxy(c *cli.Context) {
 
 	go b.Watch(client)
 
-	var listener net.Listener
-	if target != 0 {
-		log.Printf("Proxying for keys in %s on host %s in namespace %d", path, addr, target)
-		listener, err = nameNet.ListenProcessNamespace(uintptr(target), "tcp", addr)
-	} else {
-		log.Printf("Proxying for keys in %s on host %s in namespace %d", path, addr, namespacePath)
-		listener, err = nameNet.ListenNamespace(namespacePath, "tcp", addr)
-	}
+	log.Printf("Proxying for keys in %s on host %s in namespace %d", path, addr, namespacePath)
+	listener, err := nameNet.ListenNamespace(namespacePath, "tcp", addr)
+
 	if err != nil {
 		panic(err)
 	}
@@ -89,11 +93,11 @@ func main() {
 	app.Name = "nsproxy"
 	app.Usage = "Proxy into a network namespace"
 	app.Flags = []cli.Flag{
-		cli.IntFlag{"target, t", 0, "target namespace pid", false},
-		cli.StringFlag{"path, p", "", "path to a dir", false},
+		cli.IntFlag{"namespace-pid, ns-pid", 0, "target namespace pid", false},
+		cli.StringFlag{"namespace-path, ns-path", "", "target namespace path", false},
+		cli.StringFlag{"path, p", "", "path to a etcd dir", false},
 		cli.StringFlag{"addr, a", "127.0.0.1:8080", "target address inside the namespace", false},
-		cli.StringFlag{"peers, C", "http://127.0.0.1:4001", "a comma seperated list of machine addresses in the cluster", false},
-		cli.StringFlag{"namespace, ns", "", "target namespace path", false},
+		cli.StringFlag{"peers, C", "http://127.0.0.1:4001", "a comma seperated list of machine addresses in the etcd cluster", false},
 	}
 	app.Action = proxy
 	app.Run(os.Args)
